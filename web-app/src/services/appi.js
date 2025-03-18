fa7ae101bf6d209050a9142aa764e3674fc10e65
ShaswatPrakash <shaswat.p1692@gmail.com>
2025-03-18 21:30:41 +0530

    feat(api): Add API service with caching using LocalStorage

---
+++ web-app/src/services/appi.js
@@ -0,0 +1,19 @@
+import axios from "axios";
+
+const API_URL = "https://developer.nrel.gov/api/solar/solar_resource/v1.json";
+const API_KEY = "UrgPEfW3aOYcUa4f9HCz82T0nakAiqQcDPU6DIqs";
+const CACHE_KEY = "projects_cache";
+
+export const fetchProjects = async () => {
+  const cache = localStorage.getItem(CACHE_KEY);
+
+  if (cache) {
+    console.log("Using cached data...");
+    return JSON.parse(cache);
+  }
+
+  const response = await axios.get(`${API_URL}?api_key=${API_KEY}&lat=40.7128&lon=-74.0060`);
+  localStorage.setItem(CACHE_KEY, JSON.stringify(response.data));
+
+  return response.data;
+};
