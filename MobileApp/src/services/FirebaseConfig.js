7e2646c8927b66636c60aa3c938ef2fb3ec61538
ShaswatPrakash <shaswat.p1692@gmail.com>
2025-03-18 22:36:42 +0530

    feat(firebase): Configure Firebase services

---
+++ MobileApp/src/services/FirebaseConfig.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "MFA8OVhzblYCLhXedT5eMSlGal5wPEg",
+  authDomain: "web-app-e3795.firebaseapp.com",
+  projectId: "web-app-e3795",
+  storageBucket: "web-app-e3795.firebasestorage.app",
+  messagingSenderId: "177163683415",
+  appId: "177163683415:web:9a1e9bf60198bad04e8512"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
